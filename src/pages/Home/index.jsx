35aa34d84130eceed7d35afee7914df2dece2ea9
oliamanda <ferreiraamandaoliveiras@gmail.com>
2024-03-06 17:53:50 -0300

    Teste de git

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -87,6 +87,9 @@ function Home() {
         <div className="itens">
           <img
             src="https://www.bhphotovideo.com/images/images2500x2500/apple_mj2x2ll_a_watch_sport_smartwatch_38mm_1187194.jpg"
+
+
+            
             width={200}
           />
         </div>
