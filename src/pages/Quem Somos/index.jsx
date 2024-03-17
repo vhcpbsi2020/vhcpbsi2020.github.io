84ae5820ff71d4c897ac13917de928409a7e6428
wellmattos23 <wellygntonc@gmail.com>
2024-03-17 14:44:33 -0300

    alteração_texto

--- src/pages/Quem Somos/index.jsx
+++ src/pages/Quem Somos/index.jsx
@@ -9,8 +9,8 @@ function PageQuemSomos() {
                 </span>
             </h1>
             <p>
-                O Observatório Madeireiro do Baixo Amazonas (OMBA) é uma iniciativa inovadora que reúne a UFOPA, órgãos ambientais e a comunidade para promover a gestão florestal sustentável na região. Através da coleta e análise de dados, O OMBA disponibiliza dados abertos e transparentes sobre o setor madeireiro, permitindo o acompanhamento da sociedade civil e o fortalecimento da governança ambiental na região.
-                Ao fornecer informações precisas e fomentar o diálogo entre os diversos atores envolvidos, o Observatório Madeireiro do Baixo Amazonas contribui para a construção de um futuro mais sustentável para a floresta amazônica e para as comunidades que dela dependem.
+                <b>O Observatório Madeireiro do Baixo Amazonas (OMBA) é uma iniciativa inovadora que reúne a UFOPA, órgãos ambientais e a comunidade para promover a gestão florestal sustentável na região. Através da coleta e análise de dados, O OMBA disponibiliza dados abertos e transparentes sobre o setor madeireiro, permitindo o acompanhamento da sociedade civil e o fortalecimento da governança ambiental na região.
+                Ao fornecer informações precisas e fomentar o diálogo entre os diversos atores envolvidos, o Observatório Madeireiro do Baixo Amazonas contribui para a construção de um futuro mais sustentável para a floresta amazônica e para as comunidades que dela dependem.</b>
             </p>
         </section>
     )
