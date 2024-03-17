64ba65cfead25574215857a3803f6c69c64397de
wellmattos23 <wellygntonc@gmail.com>
2024-03-17 18:39:29 -0300

    update

--- src/pages/Estatisticas/index.jsx
+++ src/pages/Estatisticas/index.jsx
@@ -10,6 +10,10 @@ import PageEstrada from "../../components/cardsEstatisticas/PageEstrada";
 
 const options = [
   {
+    value: "agosto21-julho22",
+    label: "Agosto 2021 a Julho 2022",
+  },
+  {
     value: "agosto20-julho21",
     label: "Agosto 2020 a Julho 2021",
   },
@@ -36,7 +40,7 @@ const options = [
 ];
 
 function PageMapa() {
-  const [periodo, setPeriodo] = useState("agosto20-julho21");
+  const [periodo, setPeriodo] = useState("agosto21-julho22");
 
   const onChange = (value) => {
     setPeriodo(value);
@@ -56,10 +60,10 @@ function PageMapa() {
       <Container>
         <div>
           <h1 style={{ fontSize: 'clamp(19px, 4vw, 60px', fontWeight: 600, textAlign: "center" }}>
-            Exploração Madereira
+            Exploração Madeireira
           </h1>
 
-          {periodo == "agosto20-julho21" || periodo == undefined ? (
+          {periodo == "agosto21-julho22" || periodo == undefined ? (
             <p
               style={{
                 fontSize: 20,
@@ -68,7 +72,18 @@ function PageMapa() {
                 color: "#506635",
               }}
             >
-              Agosto20-Julho21 <CalendarMonthIcon />
+              Agosto de 2021 a Julho de 2022 <CalendarMonthIcon />
+            </p>
+          ) : periodo == "agosto2020-julho2021" ? (
+            <p
+                style={{
+                fontSize: 20,
+                fontWeight: 600,
+                textAlign: "center",
+                color: "#506635",
+              }}
+            >
+              Agosto de 2020 a Julho de 2021 <CalendarMonthIcon />
             </p>
           ) : periodo == "agosto19-julho20" ? (
             <p
@@ -79,7 +94,7 @@ function PageMapa() {
                 color: "#506635",
               }}
             >
-              Agosto19-Julho20 <CalendarMonthIcon />
+              Agosto de 2019 a Julho de 2020 <CalendarMonthIcon />
             </p>
           ) : periodo == "2018-2019" ? (
             <p
@@ -135,116 +150,147 @@ function PageMapa() {
             options={options}
             onChange={onChange}
             placeholder="Selecione um periodo"
-            defaultValue="agosto20-julho21"
+            defaultValue="agosto21-julho22"
             style={{ width: 220, marginLeft: 8 }}
           />
         </div>
         <Divider />
         <Main>
-          {periodo == "agosto20-julho21" || periodo == undefined ? (
+          {periodo == "agosto21-julho22" || periodo == undefined ? (
             <>
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2023/12/Inf_SIMEX_AMAZONIA_2022_PT_A4-1536x1086.jpg"
+                title={"Mapeamento da Exploração na Amazônia"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2024/01/Inf_SIMEX_ACRE_2022_PT_A5-1024x724.jpg"
+                title={"Mapeamento da Exploração no Estado do Acre"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2023/12/Inf_SIMEX_AMAZONAS_2022_PT_A4-1536x1086.jpg"
+                title={"Mapeamento da Exploração no Estado do Amazonas"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2024/01/Inf_SIMEX_MATOGROSSO_2022_PT_A4-1024x724.jpg"
+                title={"Mapeamento da Exploração no Estado do Mato Grosso"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2023/12/Inf_SIMEX_PARA_2022_PT_A4-1536x1086.jpg"
+                title={"Mapeamento da Exploração no Estado do Pará"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2024/01/Inf_SIMEX_RONDONIA_2022_PT_A4-1024x724.jpg"
+                title={"Mapeamento da Exploração no Estado de Rondônia"}
+              />
+              <CardEstatistica 
+                image="https://imazon.org.br/wp-content/uploads/2024/01/Inf_SIMEX_RORAIMA_2022_PT_A4-1024x724.jpg"
+                title={"Mapeamento da Exploração no Estadado de Roraima"}
+              />
+            </>
+          ) : periodo == "agosto20-julho21" ? (
+              <>
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-Amazonia-Agosto-2021-a-Julho-2021-IMAGEM-scaled.jpg"
-                title={"Mapeamento da exploração na Amazônia"}
+                title={"Mapeamento da Exploração na Amazônia"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Infografico_SIMEX_ACRE_ag2020_jul2021_A4-1-scaled.jpg"
-                title={"Mapeamento da exploração no Acre"}
+                title={"Mapeamento da Exploração no Estado do Acre"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-Amazonas-Agosto-2021-a-Julho-2021-IMAGEM-scaled.jpg"
-                title={"Mapeamento da exploração no Estado do Amazonas"}
+                title={"Mapeamento da Exploração no Estado do Amazonas"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-Mato-Grosso-Agosto-2021-a-Julho-2021-IMAGEM-1536x1086.jpg"
-                title={"Mapeamento da exploração em Mato Grosso"}
+                title={"Mapeamento da Exploração no Estadod do Mato Grosso"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-Agosto-2020-a-Julho-2021-1536x1086.jpg"
-                title={"Mapeamento da exploração no Pará"}
+                title={"Mapeamento da Exploração no Estado do Pará"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-Rondonia-Agosto-2021-a-Julho-2021-IMAGEM-1536x1086.jpg"
-                title={"Mapeamento da exploração em Rondonia"}
+                title={"Mapeamento da Exploração no Estado de Rondônia"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/09/Simex-RR-2022-1536x1037.png"
-                title={"Mapeamento da exploração em Roraima"}
+                title={"Mapeamento da Exploração no Estado de Roraima"}
               />
-            </>
+              </>
           ) : periodo == "agosto19-julho20" ? (
             <>
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/09/Simex_Amazonia_2022-1536x1086.jpg"
-                title={"Mapeamento da exploração na Amazônia"}
+                title={"Mapeamento da Exploração na Amazônia"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2022/04/Simex-Acre-Agosto-de-2019-a-Julho-de-2020.png"
-                title={"Mapeamento da exploração no Acre"}
+                title={"Mapeamento da Exploração no Estado do Acre"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/10/Simex_AM_Agosto2019-Julho2020.png"
-                title={"Mapeamento da exploração no Estado do Amazonas"}
+                title={"Mapeamento da Exploração no Estado do Amazonas"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/10/Simex_MT_Agosto2019-Julho2020.jpg"
-                title={"Mapeamento da exploração em Mato Grosso"}
+                title={"Mapeamento da Exploração no Estado do Mato Grosso"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/09/Simex_Para_2019-2020.jpeg"
-                title={"Mapeamento da exploração no Pará"}
+                title={"Mapeamento da Exploração no Estado do Pará"}
               />
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/10/Simex_Rondonia_Agosto2019-Julho2020_Errata-1536x1086.jpg"
-                title={"Mapeamento da exploração em Rondonia"}
+                title={"Mapeamento da Exploração no Estado de Rondônia"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/10/Simex_Roraima_Agosto2019-Julho2020.jpg"
-                title={"Mapeamento da exploração em Roraima"}
+                title={"Mapeamento da Exploração no Estado de Roraima"}
               />
 
               <CardEstatistica
                 image="https://imazon.org.br/wp-content/uploads/2021/09/Simex_AP_Agosto2019-Julho2020.png"
-                title={"Mapeamento da exploração no Amapá"}
+                title={"Mapeamento da Exploração no Estado do Amapá"}
               />
             </>
           ) : periodo == "2018-2019" ? (
             <>
             <CardEstatistica
               image="https://imazon.org.br/wp-content/uploads/2021/05/Infografico_SIMEX_2018_2019_A5-1-1536x1086.jpg"
-              title={"Mapeamento da exploração no Pará"}
+              title={"Mapeamento da Exploração no Estado do Pará"}
             />
             </>
           ) : periodo == "2017-2018" ? (
             <>
             <CardEstatistica 
               image="https://imazon.org.br/wp-content/uploads/2020/04/SIMEX_madeira_2017-2018.jpg"
-              title={"Mapeamento da exploração no Pará"}
+              title={"Mapeamento da Exploração no Estado do Pará"}
             />
             </>
           ) : periodo == "2016-2017" ? (
             <>
             <CardEstatistica 
               image="https://imazon.org.br/wp-content/uploads/2019/03/infograficosimex2016-2017.jpg"
-              title={"Mapeamento da exploração no Pará"}
+              title={"Mapeamento da Exploração no Estado do Pará"}
             />
             </>
           ) : periodo == "2015-2016" ? (
             <>
             <CardEstatistica
               image="https://imazon.org.br//wp-content/uploads/2017/11/Infografico-Simex-Par%C3%A1-2015-20161.jpg"
-              title={"Mapeamento da exploração no Pará"}
+              title={"Mapeamento da Exploração no Estado do Pará"}
             />
             </>
           ) : (
