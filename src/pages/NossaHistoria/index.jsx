02b8e37ebbc5a6c3204c0f669a4887293d0a99d0
wellmattos23 <wellygntonc@gmail.com>
2024-03-18 17:09:17 -0300

    up

--- src/pages/NossaHistoria/index.jsx
+++ src/pages/NossaHistoria/index.jsx
@@ -1,12 +1,19 @@
-import NewMap from "../../components/map";
 
+import './style.css'
 
-function NossaHistoria () {
-    return(
-    <>
-    <NewMap> </NewMap>
-    </>
+function PageQuemSomos() {
+    return (
+        <section className='text_container'>
+            <h1>
+                <span>
+                    <b>Quem Somos</b>
+                </span>
+            </h1>
+            <p>
+                <b>O Observatório Madeireiro do Baixo Amazonas (OMBA) é uma iniciativa inovadora que reúne a UFOPA, órgãos ambientais e a comunidade para promover a gestão florestal sustentável na região. Através da coleta e análise de dados, O OMBA disponibiliza dados abertos e transparentes sobre o setor madeireiro, permitindo o acompanhamento da sociedade civil e o fortalecimento da governança ambiental na região.
+                Ao fornecer informações precisas e fomentar o diálogo entre os diversos atores envolvidos, o Observatório Madeireiro do Baixo Amazonas contribui para a construção de um futuro mais sustentável para a floresta amazônica e para as comunidades que dela dependem.</b>
+            </p>
+        </section>
     )
 }
-
-export default NossaHistoria;
+export default PageQuemSomos;
