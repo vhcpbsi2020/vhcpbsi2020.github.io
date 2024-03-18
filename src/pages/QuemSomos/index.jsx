d1d8d40520ba37fdc5e145f265070359bbe71289
wellmattos23 <wellygntonc@gmail.com>
2024-03-18 17:49:05 -0300

    up

--- src/pages/QuemSomos/index.jsx
+++ src/pages/QuemSomos/index.jsx
@@ -10,7 +10,7 @@ function PageQuemSomos() {
                 </span>
             </h1>
             <p>
-                O Observatório Nacional da Amazônia Legal (OAL) é uma iniciativa inovadora que reúne a UFOPA, órgãos ambientais e a comunidade para promover a gestão florestal sustentável na região. Através da coleta e análise de dados, O OAL disponibiliza dados abertos e transparentes sobre o setor madeireiro, permitindo o acompanhamento da sociedade civil e o fortalecimento da governança ambiental na região.
+                O Observatório Nacional da Amazônia Legal (OAL) é uma iniciativa inovadora que reúne a UFOPA, órgãos ambientais e a comunidade para promover a gestão florestal sustentável na região. Através da coleta e análise de dados, o OAL disponibiliza dados abertos e transparentes sobre o setor madeireiro, permitindo o acompanhamento da sociedade civil e o fortalecimento da governança ambiental na região.
                 Ao fornecer informações precisas e fomentar o diálogo entre os diversos atores envolvidos, o Observatório Nacional da Amazônia Legal contribui para a construção de um futuro mais sustentável para a floresta amazônica e para as comunidades que dela dependem.
             </p>
         </section>
